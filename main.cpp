570582338badaf36020615d94f50f4ec562116b1
FakeGath <givanalvaradoath@gmail.com>
2024-08-17 12:38:59 -0600

    some functions as exp pow and sqrt were added

--- main.cpp
+++ main.cpp
@@ -23,6 +23,23 @@ int main(){
     std::cout<<"Abs value of Weight: "<<std::abs(weight)<<std::endl;
     std::cout<<"Abs value of savings: "<<std::abs(savings)<<std::endl;
 
+    //exp : f(x) = e ^ X where e = euler 2.718281
 
+    double exponential = std::exp(10);
+
+    std::cout<<"Exponential : "<< exponential<< std :: endl;
+    
+    //pow is to elavate a number to another
+
+    std::cout<< " 3 ^ 4 is: "<<std::pow(3,4)<< std::endl;
+    std::cout<< " 9 ^ 3 is: "<<std::pow(3,4)<< std::endl;
+
+    //log is the logaritmus to reac a number based on Euler
+
+    std::cout<<"To get 1000, you´d need to elavete to 10 to the power of: "<<std::log10(10000)<<std::endl;
+
+    //sqr 
+
+    std::cout<<"sqr root of 64 is "<<std::sqrt(64)<<std::endl;
     return 0;
 }
